b3c690d92a9cc6811d273fb6d91ab8fb3a765ddd
ASteinheiser <asteinhe@asu.edu>
2016-03-08 12:17:26 -0700

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp        = require('gulp');
+var clean       = require('gulp-clean');
+var jsonCombine = require('gulp-jsoncombine');
+var connect     = require('gulp-connect');
+var cors        = require('cors');
+
+gulp.task('clean', function() {
+  return gulp.src('dist', {
+    read: false
+  }).pipe(clean());
+});
+
+gulp.task('build', function() {
+  return gulp.src('./tool-schemas/**/*.json').pipe(jsonCombine('schemaRegistry.json', function(data) {
+    return new Buffer(JSON.stringify(data, null, 2));
+  })).pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function() {
+  return gulp.watch(['./tool-schemas/**/*.json'], ['build']);
+});
+
+gulp.task('server', function() {
+  return connect.server({
+    root: ['dist'],
+    port: process.env.PORT || 1337,
+    middleware: function() {
+      return [cors()];
+    }
+  });
+});
+
+gulp.task('default', ['clean', 'build', 'watch', 'server']);
